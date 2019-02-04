f98af8b50e0ece0cff5eb38d3a64ef1e4223742e
Samuel Aguilar <samuel.aguilar@ge.com>
2019-02-04 15:53:04 -0600

    Add routes,  router-outlet, routerLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ConversationComponent } from './conversation/conversation.component';
+import { ProfileComponent } from './profile/profile.component';
+import { Routes, RouterModule } from '@angular/router';
 
+const appRoutes : Routes = [
+  {path:'', component:HomeComponent}
+  {path:'home', component:HomeComponent}
+  {path:'login', component:LoginComponent}
+  {path:'converstaion', component:ConversationComponent},
+  {path:'profile', component:ProfileComponent}  
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ConversationComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
